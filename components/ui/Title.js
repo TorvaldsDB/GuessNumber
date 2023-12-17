8e497ae16b97863ac5ab6f072b3599da0aecfdf2
TorvaldsDu <torvalds@TorvaldsDus-MacBook-Pro.local>
2023-12-17 11:18:55 +0800

    feat: implement fonts

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Colors from "../../constants/colors";
 
 const Title = ({ children }) => {
   return (
@@ -14,9 +13,10 @@ export default Title;
 
 const styles = StyleSheet.create({
   title: {
+    // fontWeight: "bold",
+    fontFamily: "open-sans-bold",
     color: "white",
     fontSize: 24,
-    fontWeight: "bold",
     textAlign: "center",
     borderWidth: 2,
     borderColor: "white",
